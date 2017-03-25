3e274567a70333053e7f313029394f52bc44eec3
purerosefallen <78877@qq.com>
2017-03-25 15:30:44 +0800

    koishi

--- c37564826.lua
+++ c37564826.lua
@@ -3,7 +3,7 @@ local m=37564826
 local cm=_G["c"..m]
 --if not pcall(function() require("expansions/script/c37564765") end) then require("script/c37564765") end
 function cm.initial_effect(c)
-	senya.lfus(c,m,Card.IsFusionType,TYPE_FUSION)
+	senya.lfus(c,m,cm.mfilter)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
@@ -12,3 +12,6 @@ function cm.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 cm.custom_ctlm_3L=2
+function cm.mfilter(c)
+	return senya.check_fusion_set_3L(c) and c:IsFusionType(TYPE_FUSION)
+end
