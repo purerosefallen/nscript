fd4e637593567c5f02f8e9008bf5bfce45b98066
purerosefallen <78877@qq.com>
2017-04-11 23:59:04 +0800

    Update c37564027.lua
    Fix typo

--- c37564027.lua
+++ c37564027.lua
@@ -78,7 +78,7 @@ function cm.xmfilter(c)
 	return senya.check_set_elem(c) and c:IsType(TYPE_XYZ) and c:GetRank()==4
 end
 function cm.rcon(e,tp,eg,ep,ev,re,r,rp)
-	return bit.band(r,REASON_COST)~=0 and re:IsHasType(0x7e0) and re:IsActiveType(TYPE_XYZ) and re:GetHandler():IsSetCard(0x770) and e:GetHandler():GetOverlayGroup():IsExists(cm.xmfilter,1,nil)
+	return bit.band(r,REASON_COST)~=0 and re:IsHasType(0x7e0) and re:IsActiveType(TYPE_XYZ) and senya.check_set_elem(re:GetHandler()) and e:GetHandler():GetOverlayGroup():IsExists(cm.xmfilter,1,nil)
 and re:GetHandler()~=e:GetHandler()
 end
 
